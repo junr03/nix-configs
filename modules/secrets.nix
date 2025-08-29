074ca2683979698453017023862ef507dd13eb32
José Ulises Niño Rivera <junr03@users.noreply.github.com>
2025-08-29 07:58:20 -0700

    update to ssh key
    Signed-off-by: José Ulises Niño Rivera <junr03@users.noreply.github.com>

--- modules/secrets.nix
+++ modules/secrets.nix
@@ -10,26 +10,24 @@ let
 in
 {
   age.identityPaths = [
-    "/Users/${user}/.age/identities/yubikey_5c.txt"
-    "/Users/${user}/.age/identities/yubikey_5c_nano.txt"
-    "/Users/${user}/.age/identities/yubikey_5c_nfc.txt"
+    "/Users/${user}/.ssh/nix_configs_identity"
   ];
 
-  age.secrets."github-ssh-key" = {
-    symlink = true;
-    path = "/Users/${user}/.ssh/github";
-    file = ../secrets/github-ssh-key.age;
-    mode = "600";
-    owner = "${user}";
-    group = "staff";
-  };
+  # age.secrets."github-ssh-key" = {
+  #   symlink = true;
+  #   path = "/Users/${user}/.ssh/github";
+  #   file = ../secrets/github-ssh-key.age;
+  #   mode = "600";
+  #   owner = "${user}";
+  #   group = "staff";
+  # };
 
-  age.secrets."electricpeak-ssh-key" = {
-    symlink = true;
-    path = "/Users/${user}/.ssh/electricpeak";
-    file = ../secrets/electricpeak-ssh-key.age;
-    mode = "600";
-    owner = "${user}";
-    group = "staff";
-  };
+  # age.secrets."electricpeak-ssh-key" = {
+  #   symlink = true;
+  #   path = "/Users/${user}/.ssh/electricpeak";
+  #   file = ../secrets/electricpeak-ssh-key.age;
+  #   mode = "600";
+  #   owner = "${user}";
+  #   group = "staff";
+  # };
 }
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,14 +1,7 @@
-let
-  yubikey_5c = "age1yubikey1qt8yzdrg5tf6me6kdppcjgtnft2rls33q0qdrud3p3j0nsup9jjm29smyhp";
-  yubikey_5c_nano = "age1yubikey1q0gasxft59c42dehv996kahtngdvrw298nlmyzmenwdkw8vs8aw6us6j5e2";
-  yubikey_5c_nfc = "age1yubikey1q0qtk930wupw6r0apvkmm7ulkzzw67tu6mxr8xfzpzh9jdmt24c6wj6juzd";
-  yubikeys = [
-    yubikey_5c
-    yubikey_5c_nano
-    yubikey_5c_nfc
-  ];
-in
-{
-  "electricpeak-ssh-key.age".publicKeys = yubikeys;
-  "github-ssh-key.age".publicKeys = yubikeys;
-}
+# let
+#   nix-configs-identity-pub-key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOJJiMIB5tmAtls2/Z9KhLKA9BrMSUlSHHsPW73clu2a inbox@junr03.com";
+# in
+# {
+#   "electricpeak-ssh-key.age".publicKeys = [ nix-configs-identity-pub-key ];
+#   "github-ssh-key.age".publicKeys = [ nix-configs-identity-pub-key ];
+# }
