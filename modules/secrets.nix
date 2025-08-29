0b04eba569ca5fbce966ec6e7152dafb024eac44
José Ulises Niño Rivera <junr03@users.noreply.github.com>
2025-08-29 08:02:07 -0700

    secrets with ssh
    Signed-off-by: José Ulises Niño Rivera <junr03@users.noreply.github.com>

--- modules/secrets.nix
+++ modules/secrets.nix
@@ -13,21 +13,21 @@ in
     "/Users/${user}/.ssh/nix_configs_identity"
   ];
 
-  # age.secrets."github-ssh-key" = {
-  #   symlink = true;
-  #   path = "/Users/${user}/.ssh/github";
-  #   file = ../secrets/github-ssh-key.age;
-  #   mode = "600";
-  #   owner = "${user}";
-  #   group = "staff";
-  # };
+  age.secrets."github-ssh-key" = {
+    symlink = true;
+    path = "/Users/${user}/.ssh/github";
+    file = ../secrets/github-ssh-key.age;
+    mode = "600";
+    owner = "${user}";
+    group = "staff";
+  };
 
-  # age.secrets."electricpeak-ssh-key" = {
-  #   symlink = true;
-  #   path = "/Users/${user}/.ssh/electricpeak";
-  #   file = ../secrets/electricpeak-ssh-key.age;
-  #   mode = "600";
-  #   owner = "${user}";
-  #   group = "staff";
-  # };
+  age.secrets."electricpeak-ssh-key" = {
+    symlink = true;
+    path = "/Users/${user}/.ssh/electricpeak";
+    file = ../secrets/electricpeak-ssh-key.age;
+    mode = "600";
+    owner = "${user}";
+    group = "staff";
+  };
 }
